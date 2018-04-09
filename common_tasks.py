e157dbc7c06b6f3334174306461d56def1ec875f
Comm4nd0 <marcobaldanza@hotmail.co.uk>
2018-04-09 14:31:07 +0100

    many things, all good ones!

--- common_tasks.py
+++ common_tasks.py
@@ -6,24 +6,81 @@ import sys
 import subprocess
 import getpass
 import pexpect
+import shutil
 
-def print_menu():
+pswd = ''
+
+def print_ascii():
     print("""
-       _____                                        _______        _         
-      / ____|                                      |__   __|      | |        
-     | |     ___  _ __ ___  _ __ ___   ___  _ __      | | __ _ ___| | _____  
-     | |    / _ \| '_ ` _ \| '_ ` _ \ / _ \| '_ \     | |/ _` / __| |/ / __| 
-     | |___| (_) | | | | | | | | | | | (_) | | | |    | | (_| \__ \   <\__ \ 
-      \_____\___/|_| |_| |_|_| |_| |_|\___/|_| |_|    |_|\__,_|___/_|\_\___/ 
-                                                                             
+           _____                                        _______        _         
+          / ____|                                      |__   __|      | |        
+         | |     ___  _ __ ___  _ __ ___   ___  _ __      | | __ _ ___| | _____  
+         | |    / _ \| '_ ` _ \| '_ ` _ \ / _ \| '_ \     | |/ _` / __| |/ / __| 
+         | |___| (_) | | | | | | | | | | | (_) | | | |    | | (_| \__ \   <\__ \ 
+          \_____\___/|_| |_| |_|_| |_| |_|\___/|_| |_|    |_|\__,_|___/_|\_\___/ 
+    """)
+
+def print_menu(status):
+    print_ascii()
+    print("""
                                                                          
     [0] - Restart Network-Manager
     [1] - Start PyCharm
     [99]- Exit
     """)
+    if status:
+        print('Info: {}\n'.format(status))
+
+def ansible_installed():
+    global pswd
+    yes_ans = ['Y', 'y', '']
+    if not shutil.which('ansible'):
+        print_ascii()
+        print('You must install Ansible for this tool to work!\n')
+        print('Would you like to install it now?')
+        while True:
+            try:
+                ans = input('[Y/n] ')
+                if ans in yes_ans:
+                    os.system('clear')
+                    print_ascii()
+                    print('Installing Ansible')
+                    pswd = getpass.getpass('sudo password:')
+                    child = pexpect.spawn('sudo apt install ansible')
+                    child.expect(':')
+                    child.sendline(pswd)
+                    child.expect(']')
+                    child.sendline('Y')
+                    child.interact()
+                    break
+                elif ans == 'n':
+                    print('\nPlease install Ansible and restart the Common Tasks program!')
+                    sys.exit()
+                else:
+                    print("Not a valid input, please enter (Y)es or (n)o.")
+            except KeyboardInterrupt:
+                print("""
+Oh i see how it is! Goodbye!! 
+     .-',',''-.
+   .'          '.
+  /   O      O   |
+ :           `    :
+ |                |   
+ :    .------.    :
+  \  '        '  /
+   '.          .'
+     '-......-'
+""")
+                sys.exit()
 
 def restart_nm():
-    pswd = getpass.getpass('sudo password:')
+    global pswd
+    if not pswd:
+        try:
+            pswd = getpass.getpass('sudo password:')
+        except KeyboardInterrupt:
+            print("\n\nIt's ok I didn't want to know you password anyway. Goodbye (⌣_⌣”)")
+            sys.exit()
     child = pexpect.spawn('sudo systemctl restart network-manager.service')
     child.expect(':')
     child.sendline(pswd)
@@ -34,24 +91,30 @@ def pycharm():
     subprocess.Popen(['bash', file_path, '&'])
 
 def main():
+    status = ''
     while True:
         os.system('clear')
-        print_menu()
-        option = input("Selection: ")
-
+        print_menu(status)
         try:
-            option = int(option)
-            return option
-        except:
-            print("Not a valid option")
-            time.sleep(2)
+            option = input("Selection: ")
+
+            try:
+                option = int(option)
+                return option
+
+            except ValueError:
+                status = "Not a valid option"
+        except KeyboardInterrupt:
+            status = "Hit '99' to exit, my friend."
 
 if __name__ == '__main__':
+    os.system('clear')
+    ansible_installed()
     while True:
-        option = main()
-        if option == 99:
-            sys.exit()
-        elif option == 0:
-            restart_nm()
-        elif option == 1:
-            pycharm()
+       option = main()
+       if option == 99:
+           sys.exit()
+       elif option == 0:
+           restart_nm()
+       elif option == 1:
+           pycharm()
